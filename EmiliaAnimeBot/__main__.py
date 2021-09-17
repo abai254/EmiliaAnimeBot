98507b61b5812599bb872312af3a95532549634a
abai254 <84307584+abai254@users.noreply.github.com>
2021-09-17 16:20:14 +0530

    Update __main__.py

--- EmiliaAnimeBot/__main__.py
+++ EmiliaAnimeBot/__main__.py
@@ -56,7 +56,7 @@ def get_readable_time(seconds: int) -> str:
 PM_START_TEXT = """
 `Hey There!` [👩‍💼](https://telegra.ph/file/c766d686089a43e0ff33a.jpg) 
 `My name is` *Emilia*`
-`I am an Anime Themed group management bot.`
+`I am anh Anime Themed group management bot.`
 You can find my list of available commands with` *🔐Commands*   
 """
 
